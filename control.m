ecee3f25f0f761d57b83bc062c5cb6f67e1683da
Asmaa-204 <asmaa.abozaid204@gmail.com>
2025-05-05 04:00:13 +0300

    report almost done

--- control.m
+++ control.m
@@ -1,16 +1,23 @@
 A1 = 5; A2 = 4; R1 = 3; R2 = 5;
 cc_eqn = [A1*A2*R1*R2, A1*R1 + A2*R2 + A1*R2, 1];
 
-%transfer functions
-Qo_Qin = tf([1], cc_eqn);
-Q1_Qin = tf([A2*R2, 1], cc_eqn);
-H1_Qin = tf([A2*R2, R1+R2], cc_eqn);
-H2_Qin = tf([R2], cc_eqn);
+%1,2. transfer functions
+Qo_Qin = tf([1], cc_eqn)
+Q1_Qin = tf([A2*R2, 1], cc_eqn)
+H1_Qin = tf([A2*R2, R1+R2], cc_eqn)
+H2_Qin = tf([R2], cc_eqn)
 
-%study system stability
-poles = roots(cc_eqn);
+%3. study system stability
+poles = roots(cc_eqn)
 
-%step responses
+%visualize
+figure;
+pzmap(Qo_Qin);
+grid on;
+title('Pole-Zero Plot of the System');
+
+
+%4. step responses
 figure;
 step(Qo_Qin);
 title('Step Response: Qo/Qin');
@@ -27,13 +34,18 @@ figure;
 step(H2_Qin);
 title('Step Response: H2/Qin');
 
-%5. track hd
+%5,6. track hd
 t = 0:0.1:100;
-Hd_H2 = feedback(H2_Qin, 1);
+Hd_H2 = feedback(H2_Qin, 1)
 hd = 5;
 figure;
 [y_H2, t] = step(Hd_H2, t);
 y_H2 = hd * y_H2;
+
+
+% 7. get h2 transient response
+step(5*Hd_H2);
+stepinfo(5*Hd_H2)
 
 % Compute H1 response
 Qin = (hd - y_H2);
@@ -44,7 +56,7 @@ legend('h_2', 'h_1', 'h_d');
 title('Response to h_d = 5m (P-Control)');
 grid on;
 
-% different kp values
+% 8,9. different kp values
 hd = 5;
 Kp_values = [1, 10, 100];
 figure; hold on;
@@ -56,4 +68,22 @@ ess = hd - y(end);
 plot(t, y, 'DisplayName', sprintf('Kp = %d (e_{ss} = %.5f)', Kp, ess));
 end
 title('Effect of K_p on Tracking and e_{ss}');
-legend; grid on;
+legend; grid on; hold off;
+
+
+% 10. PID control
+Kp =20;   % Proportional gain
+KI = 1;   % Integral gain
+Kd = 150;  % Derivative gain
+
+% PID controller transfer function
+C = tf([Kd Kp KI], [1 0]);  % Represents Kd*s + Kp + KI/s
+
+Hd_H2 = feedback(C * H2_Qin, 1)
+
+step(6 * Hd_H2);  % Scale for 6m setpoint
+stepinfo(6 * Hd_H2)
+title(sprintf('PID Control: Kp=%.2f, KI=%.2f, Kd=%.2f', Kp, KI, Kd));
+xlabel('Time (s)');
+ylabel('Height (m)');
+grid on;
